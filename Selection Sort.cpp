dca0b64eeafcbcfa281b9468fc301d79466c0240
Aman Gupta <49602858+1aman1@users.noreply.github.com>
2019-07-03 11:44:13 +0530

    Add files via upload

---
+++ Selection Sort.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+void swap(int * x, int*y) {
+  int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+/* concept draws sense by preparing a sorted and an unsorted sequence
+ * it repeatedly finds minimum in sequence, 
+ * and puts it in sorted sequence
+ * [ [SORTED] [UNSORTED] ]
+ * */
+void selection(int ar[], int Len) {	
+	for(int i = 0; i<Len ; ++i) {
+    int min = i;
+    for(int j = i+1; j < Len; ++j) {	
+      if(ar[j] < ar[min])
+        min = j;
+    }
+    if(min != i)
+      swap(&ar[min], &ar[i]);
+  }
+}
+
+int main() {
+  int *arr, n;
+	cout << "\nlen of sequence :";
+	cin >> n;
+  arr = new int[n];
+  for(int i = 0; i<n ; ++i)          
+    cin >> arr[i];
+
+  selection(arr, n);
+
+  for(int i = 0; i<n; ++i)         
+    cout << arr[i] << " ";
+  cout << endl;
+  //	system("pause");
+  return 9;
+}
